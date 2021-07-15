c15495295ac4acdbf1a8c0808bfcf1ee1f5f9aa4
Akiaksandra <raa12345@mail.ru>
2021-07-15 16:59:16 +0300

    fix some styles

--- src/components/filters-for-user/filters.tsx
+++ src/components/filters-for-user/filters.tsx
@@ -55,7 +55,7 @@ const Filters:React.FC = () => {
           <input className="filter-price-input" type="number" min="1" onBlur={handleChangeMaxPrice}></input>
         </div>
         <div className = "manufactures-container">
-          <ButtonComponent text="Производитель" />
+          <ButtonComponent text="Производитель" style={{marginTop: "15px"}}/>
           <div className = "manufactures-list">
             <FormGroup>
               {
