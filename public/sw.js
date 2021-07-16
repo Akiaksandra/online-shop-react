08c4ece4b367fe4aaf24d15288d9329d072df0dd
Akiaksandra <raa12345@mail.ru>
2021-07-16 11:56:26 +0300

    8 try

--- public/sw.js
+++ public/sw.js
@@ -1,12 +1,12 @@
-const staticCacheName = 's-app-v3'
-const dynamicCacheName = 'd-app-v3'
+const staticCacheName = 's-app-v3';
+const dynamicCacheName = 'd-app-v3';
 
 self.addEventListener("install", (event) => {
   console.log('install')
  caches.open(staticCacheName).then((cache) => {
             cache.addAll([
               '/',
-              '/index.html',
+              'index.html',
               '/products',
               '/orders-history',
               '/cart',
