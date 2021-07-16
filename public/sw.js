bd1c2060ee69c8563ec02b72e927d6c889e2576f
Akiaksandra <raa12345@mail.ru>
2021-07-16 11:50:15 +0300

    try 7

--- public/sw.js
+++ public/sw.js
@@ -1,61 +1,44 @@
 const staticCacheName = 's-app-v3'
 const dynamicCacheName = 'd-app-v3'
 
-self.addEventListener("install", async (event) => {
-  const cache = await caches.open(staticCacheName)
-  await cache.addAll([
-    '/',
-    '/index.html',
-    '/products',
-    '/orders-history',
-    '/cart',
-      '/static/js/main.chunk.js',
-      '/static/js/vendors~main.chunk.js',
-      '/static/js/bundle.js',
-      '/static/css/main.chunk.css',
-      'https://fonts.gstatic.com/s/roboto/v27/KFOlCnqEu92Fr1MmEU9fBBc4.woff2',
-      'https://fonts.gstatic.com/s/roboto/v27/KFOmCnqEu92Fr1Mu5mxKOzY.woff2',
-      'https://fonts.gstatic.com/s/roboto/v27/KFOmCnqEu92Fr1Mu4mxK.woff2',
-      'https://fonts.gstatic.com/s/roboto/v27/KFOlCnqEu92Fr1MmEU9fABc4EsA.woff2',
-      '/favicon.ico',
-      '/manifest.json',
-      '/logo192.png',
-  ])
-  // caches.open(staticCacheName).then((cache) => {
-  //           cache.addAll([
-  //             '/',
-  //             '/index.html',
-  //             '/products',
-  //             '/orders-history',
-  //             '/cart',
-  //               '/static/js/main.chunk.js',
-  //               '/static/js/vendors~main.chunk.js',
-  //               '/static/js/bundle.js',
-  //               '/static/css/main.chunk.css',
-  //               'https://fonts.gstatic.com/s/roboto/v27/KFOlCnqEu92Fr1MmEU9fBBc4.woff2',
-  //               'https://fonts.gstatic.com/s/roboto/v27/KFOmCnqEu92Fr1Mu5mxKOzY.woff2',
-  //               'https://fonts.gstatic.com/s/roboto/v27/KFOmCnqEu92Fr1Mu4mxK.woff2',
-  //               'https://fonts.gstatic.com/s/roboto/v27/KFOlCnqEu92Fr1MmEU9fABc4EsA.woff2',
-  //               '/favicon.ico',
-  //               '/manifest.json',
-  //               '/logo192.png',
-  //           ])
-  //       })
+self.addEventListener("install", (event) => {
+  console.log('install')
+ caches.open(staticCacheName).then((cache) => {
+            cache.addAll([
+              '/',
+              '/index.html',
+              '/products',
+              '/orders-history',
+              '/cart',
+                '/static/js/main.chunk.js',
+                '/static/js/vendors~main.chunk.js',
+                '/static/js/bundle.js',
+                '/static/css/main.chunk.css',
+                'https://fonts.gstatic.com/s/roboto/v27/KFOlCnqEu92Fr1MmEU9fBBc4.woff2',
+                'https://fonts.gstatic.com/s/roboto/v27/KFOmCnqEu92Fr1Mu5mxKOzY.woff2',
+                'https://fonts.gstatic.com/s/roboto/v27/KFOmCnqEu92Fr1Mu4mxK.woff2',
+                'https://fonts.gstatic.com/s/roboto/v27/KFOlCnqEu92Fr1MmEU9fABc4EsA.woff2',
+                '/favicon.ico',
+                '/manifest.json',
+                '/logo192.png',
+            ])
+        })
 
 })
 
-// self.addEventListener('activate', async event => {
-//   const cacheNames = await caches.keys()
-//   await Promise.all(
-//     cacheNames
-//       .filter(name => name !== staticCacheName)
-//       .filter(name => name !== dynamicCacheName)
-//       .map(name => caches.delete(name))
-//   )
-// })
+self.addEventListener('activate', async event => {
+
+  const cacheNames = await caches.keys()
+  await Promise.all(
+    cacheNames
+      .filter(name => name !== staticCacheName)
+      .filter(name => name !== dynamicCacheName)
+      .map(name => caches.delete(name))
+  )
+})
 
 self.addEventListener("fetch", (event) => {
-
+  console.log('active')
     if (!navigator.onLine) {
         if (event.request.url === "http://localhost:3000/static/js/main.chunk.js") {
             event.waitUntil(
